62f5ca475823fd6f233c472724af1c4edfab323b
PauArias30 <45827625+PauArias30@users.noreply.github.com>
2023-03-29 15:58:21 +0200

    Update apply_segmentation.py

--- apply_segmentation.py
+++ apply_segmentation.py
@@ -9,8 +9,6 @@ from torch.utils.data import DataLoader
 import u_net
 import utils
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
 # to ensure reproducible training/validation split
 random.seed(42)
 
@@ -18,11 +16,11 @@ random.seed(42)
 DATA_DIR = Path.cwd() / "TrainingData"
 
 # this is my best epoch - what is yours?
-BEST_EPOCH = 30
+#BEST_EPOCH = 30
 CHECKPOINTS_DIR = Path.cwd() / "200_epochs_24_number_of_fake" / f"model.pth"
 
 # hyperparameters
-NO_VALIDATION_PATIENTS = 2
+#NO_VALIDATION_PATIENTS = 2
 IMAGE_SIZE = [64, 64]
 
 # find patient folders in training directory
@@ -32,12 +30,29 @@ patients = [
     for path in DATA_DIR.glob("*")
     if not any(part.startswith(".") for part in path.parts)
 ]
-random.shuffle(patients)
+#random.shuffle(patients)
+
+# Choose training and validation splits
+train_split = []
+validation_split = []
+
+for i in patients:
+    if 'p107' in str(i):
+        validation_split.append(i)
+    elif 'p117' in str(i): 
+        validation_split.append(i)
+    elif 'p120' in str(i):
+        validation_split.append(i)
+    else:
+        train_split.append(i)
+        
+print(train_split)
+print(validation_split)
 
 # split in training/validation after shuffling
 partition = {
-    "train": patients[:-NO_VALIDATION_PATIENTS],
-    "validation": patients[-NO_VALIDATION_PATIENTS:],
+    "train": train_split,
+    "validation": validation_split,
 }
 
 # load validation data
