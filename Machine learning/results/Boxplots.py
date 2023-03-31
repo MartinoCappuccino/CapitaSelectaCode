c1f1cf2081372acec5fe5bb33be3a7740ef77273
Jurre <78423354+JurreWeijer@users.noreply.github.com>
2023-03-31 12:36:40 +0200

    results

---
+++ Machine learning/results/Boxplots.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 30 10:28:34 2023
+
+@author: 20192757
+"""
+import matplotlib.pyplot as plt
+import statistics as st
+from scipy import stats
+from pathlib import Path
+
+score0 = []
+score = "HD"
+RESULT_PATH = Path(r"C:\Users\20182371\Documents\TUe\CapitaSelectaCode\Machine learning\results")
+with open(RESULT_PATH / "{x}_0.txt".format(x=score), "r") as f:
+  for line in f:
+      x = (line.strip())
+      score0.append(float(x))
+
+score4 = [] 
+with open(RESULT_PATH / "{x}_4.txt".format(x=score), "r") as f:
+  for line in f:
+      x = (line.strip())
+      score4.append(float(x))
+    
+score8 = [] 
+with open(RESULT_PATH /"{x}_8.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score8.append(float(x))
+
+score12 = [] 
+with open(RESULT_PATH /"{x}_12.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score12.append(float(x))
+
+score16 = [] 
+with open(RESULT_PATH /"{x}_16.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score16.append(float(x))
+
+score20 = [] 
+with open(RESULT_PATH /"{x}_20.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score20.append(float(x))
+
+score24 = [] 
+with open(RESULT_PATH / "{x}_24.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score24.append(float(x))
+
+score28 = [] 
+with open(RESULT_PATH /"{x}_28.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score28.append(float(x))
+      
+score32 = [] 
+with open(RESULT_PATH /"{x}_32.txt".format(x=score), "r") as f:
+  for line in f:
+      x = line.strip()
+      score32.append(float(x))
+
+
+
+
+# score0 = list(filter(lambda num: num != 0, score0))
+# score4 = list(filter(lambda num: num != 0, score4))
+# score8 = list(filter(lambda num: num != 0, score8))
+# score12 = list(filter(lambda num: num != 0, score8))
+# #score16 = list(filter(lambda num: num != 0, score8))
+# score20 = list(filter(lambda num: num != 0, score8))
+# #score24 = list(filter(lambda num: num != 0, score8))
+# score28 = list(filter(lambda num: num != 0, score8))
+# score32 = list(filter(lambda num: num != 0, score8))
+    
+# = [0,0,0]
+#score24 = [0,0,0]
+
+boxprops = dict(linestyle='-', linewidth=5, color='steelblue')
+whiskerprops = dict(linestyle='-', linewidth=5, color='steelblue')
+capprops = dict(linestyle='-', linewidth=5, color='black')
+medianprops = dict(linestyle='-', linewidth=5, color = 'limegreen')
+
+# DICE
+fig, ax = plt.subplots(1,1)
+fig.set_size_inches(26,18)
+fig.tight_layout(pad=10.0)
+
+#plt.boxplot(ax = ax, boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score0, positions = [1], widths=(0.6), boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score4, positions = [2], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score8, positions = [3], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score12, positions = [4], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score16, positions = [5], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score20, positions = [6], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score24, positions = [7], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score28, positions = [8], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+plt.boxplot(score32, positions = [9], widths=(0.6),boxprops=boxprops, whiskerprops=whiskerprops, capprops=capprops, medianprops=medianprops)
+
+title = f'{score} for different number of fake images'
+fig.suptitle(title, fontsize=45, fontweight='bold')
+
+plt.grid()
+
+xticks = ['0', '4', '8','12','16','20','24','28','32']
+ax.set_xticklabels(xticks)
+
+labels = ax.get_xticklabels() + ax.get_yticklabels()
+[label.set_fontweight('bold') for label in labels]
+
+ax.tick_params(axis='x', labelrotation=0, labelsize=35)
+ax.tick_params(axis='y', labelrotation=0, labelsize=40)
+
+ax.set_ylabel('DSC', fontsize=40)
+ax.set_xlabel('Number of fake images', fontsize=40)
+ax.yaxis.labelpad = 20
+ax.xaxis.labelpad = 20
+
+plt.savefig(f'boxplot_{score}_ml.png')
+   
+median0 = st.median(score0)
+median4 = st.median(score4)
+median8 = st.median(score8)
+median12 = st.median(score12)
+median16 = st.median(score16)
+median20 = st.median(score20)
+median24 = st.median(score24)
+median28 = st.median(score28)
+median32 = st.median(score32)
+median = [median0, median4, median8, median12, median16, median20, median24, median28, median32]
+print(median)
+
+std0 = st.stdev(score0)
+std4 = st.stdev(score4)
+std8 = st.stdev(score8)
+std12 = st.stdev(score12)
+std16 = st.stdev(score16)
+std20 = st.stdev(score20)
+std24 = st.stdev(score24)
+std28 = st.stdev(score28)
+std32 = st.stdev(score32)
+std = [std0, std4, std8, std12, std16, std20, std24, std28, std32]
+print(std)
+
+print(stats.wilcoxon(score0,score4))
---
+++ Machine learning/segment_ML.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 29 11:04:41 2023
+
+@author: 20192757
+"""
+import random
+from pathlib import Path
+
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from scipy.spatial.distance import directed_hausdorff
+
+import u_net
+import utils
+
+def dice_score(x, y, eps=1e-5):
+    return (2*(x*y).sum()+eps) / ((x+y).sum()+eps)
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# to ensure reproducible training/validation split
+random.seed(42)
+
+# directorys with data and to stored training checkpoints
+WORKING_DIR = Path(r"C:\Users\20182371\Documents\TUe\8DM20_CS_Medical_Imaging\DeepLearning_Project")
+
+DATA_DIR = WORKING_DIR / "TrainingData" 
+CHECKPOINTS_DIR = Path.cwd() / "segmentation_model_weights"
+
+# this is my best epoch - what is yours?
+number_fake = 32
+MODEL_DIR = CHECKPOINTS_DIR / f"60_epochs_{number_fake}_number_of_fake" / "model.pth"
+
+# hyperparameters
+NO_VALIDATION_PATIENTS = 3
+IMAGE_SIZE = [64, 64]
+
+# find patient folders in training directory
+# excluding hidden folders (start with .)
+patients = [
+    path
+    for path in DATA_DIR.glob("*")
+    if not any(part.startswith(".") for part in path.parts)
+]
+
+train_split = []
+validation_split = []
+
+for i in patients:
+    if 'p107' in str(i):
+        validation_split.append(i)
+    elif 'p117' in str(i): 
+        validation_split.append(i)
+    elif 'p120' in str(i):
+        validation_split.append(i)
+    else:
+        train_split.append(i)
+        
+# print(train_split)
+# print(validation_split)
+
+# split in training/validation after shuffling
+partition = {
+    "train": train_split,
+    "validation": validation_split,
+}
+
+# load validation data
+valid_dataset = utils.ProstateMRDataset(partition["validation"], IMAGE_SIZE)
+
+unet_model = u_net.UNet(num_classes=1)
+unet_model.load_state_dict(torch.load(MODEL_DIR, map_location='cpu'))
+unet_model.eval()
+
+# TODO
+# apply for all images and compute Dice score with ground-truth.
+# output .mhd images with the predicted segmentations
+with torch.no_grad():
+    DiceScores = []
+    HausdorffDist = []
+    for i in range(len(valid_dataset)):
+        predict_index = i
+        (input, target) = valid_dataset[predict_index]
+        output = torch.sigmoid(unet_model(input[np.newaxis, ...]))
+        
+        prediction = output
+        prediction = prediction[0,0,:,:]
+        target = target[0,:,:]
+        
+        dice = dice_score(prediction, target)
+        #if all(v == 0 for v in target):
+        DiceScores.append(dice.numpy())
+        HausdorffDist.append(directed_hausdorff(prediction.numpy(), target.numpy())[0])
+    
+    (input, target) = valid_dataset[250]
+    output = torch.sigmoid(unet_model(input[np.newaxis, ...]))
+    prediction = torch.round(output)
+    prediction = prediction[0,0,:,:]
+    target = target[0,:,:]
+    
+    fig, ax = plt.subplots(1, 3)
+    ax[0].imshow(input[0,:,:], cmap="gray")
+    ax[0].set_title("Input")
+    ax[0].axis("off")
+
+    ax[1].imshow(target)
+    ax[1].set_title("Ground-truth")
+    ax[1].axis("off")
+
+    ax[2].imshow(prediction)
+    ax[2].set_title("Prediction")
+    ax[2].axis("off")
+    plt.show()
+
+with open("DICE_{x}.txt".format(x=number_fake), "w") as f:
+    for s in DiceScores:
+        f.write(str(s) +"\n")
+
+with open("HD_{x}.txt".format(x=number_fake), "w") as f:
+    for s in HausdorffDist:
+        f.write(str(s) +"\n")
